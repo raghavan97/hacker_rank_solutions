879375feffca034a3a5f50a3ad40c6f015237740
raghavan <raghavan@riptideio.com>
2019-10-08 21:29:26 +0530

    master: Some good comments

--- new_year_chaos.py
+++ new_year_chaos.py
@@ -11,52 +11,58 @@ from pprint import pprint
 
 # https://www.hackerrank.com/challenges/new-year-chaos/problem
 
-
+# The curr_posn is the array that contains the current position
 
 
 # Complete the minimumBribes function below.
 def minimumBribes(q):
     qlen = len(q)
-    start_posn = range(1,qlen+1)
+    curr_posn = range(1,qlen+1)
 
-    no_of_swaps = 0
+    no_of_bribes = 0
 
     count = 0
     while count < qlen:
-        if q[count] == start_posn[count]:
+
+        # is the person is in the right place?
+        if q[count] == curr_posn[count]:
             count += 1
             continue
 
+        # unless we have one person in the right, we cant attempt to fix it
         if count+1 >= qlen:
             print("Too chaotic")
             return
 
-        if start_posn[count+1] == q[count]:
-            tmp = start_posn[count]
-            tmp1 = start_posn[count+1]
-            start_posn[count] = tmp1
-            start_posn[count+1] = tmp
-            no_of_swaps += 1
+        # The one to the right is the correct one, do a bribe
+        if curr_posn[count+1] == q[count]:
+            tmp = curr_posn[count]
+            tmp1 = curr_posn[count+1]
+            curr_posn[count] = tmp1
+            curr_posn[count+1] = tmp
+            no_of_bribes += 1
             continue
 
+        # the one in right did not match, check the next one in the right
         if count+2 >= qlen:
             print("Too chaotic")
             return
 
-        if start_posn[count+2] == q[count]:
-            tmp = start_posn[count]
-            tmp1 = start_posn[count+1]
-            tmp2 = start_posn[count+2]
-            start_posn[count] = tmp2
-            start_posn[count+1] = tmp
-            start_posn[count+2] = tmp1
-            no_of_swaps += 2
-
+        # the one next to the right matches, we need to bribe twice to get
+        # him to the correct position
+        if curr_posn[count+2] == q[count]:
+            tmp = curr_posn[count]
+            tmp1 = curr_posn[count+1]
+            tmp2 = curr_posn[count+2]
+            curr_posn[count] = tmp2
+            curr_posn[count+1] = tmp
+            curr_posn[count+2] = tmp1
+            no_of_bribes += 2
         else:
             print("Too chaotic")
             return
 
-    print(no_of_swaps)
+    print(no_of_bribes)
 
 
 if __name__ == '__main__':
