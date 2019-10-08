21f94bdce74638c31ca2fad159d5626af92f9497
raghavan <raghavan@riptideio.com>
2019-10-08 20:47:12 +0530

    master: All tests pass

--- minimum_swaps_2.py
+++ minimum_swaps_2.py
@@ -1,43 +1,46 @@
 #!/bin/python
 
+import math
 import os
+import random
+import re
+import sys
 
-
-def dprint(msg):
-    pass
-    print(msg)
-
-
-# Complete the minimumSwaps function below.
 def minimumSwaps(arr):
+
     alen = len(arr)
-    dprint(arr)
 
-    i = 0
-    anchor = 0
     no_of_swaps = 0
+    incorrect = dict()
+    content = dict()
 
-    while anchor < alen:
-        if arr[i] < arr[anchor]:
-            dprint("swapping {} to {}".format(arr[i], arr[anchor]))
-            tmp = arr[i]
-            arr[i] = arr[anchor]
-            arr[anchor] = tmp
+    count = 0
+    while count < alen:
+        if arr[count] != (count+1):
+            incorrect[count+1] = arr[count]
+            content[arr[count]] = count+1
+        count += 1
+
+    keys = sorted(incorrect.keys())
+    for k in keys:
+        if incorrect[k] != k:
+            #e2_index = get_ind_for(k,content)
+            e2_index = content[k]
+            tmp = incorrect[k]
+            incorrect[k] = k
+            incorrect[e2_index] = tmp
+            content[tmp] = e2_index
+            content[k] = k
             no_of_swaps += 1
-            dprint(arr)
 
-        i += 1
-
-        if i == alen:
-            anchor += 1
-            i = anchor
-            dprint("----------------")
-
-    dprint(arr)
     return no_of_swaps
 
+yy=[1, 3, 5, 2, 4, 6, 7]
+minimumSwaps(yy)
 
-if __name__ == '__main__':
+
+
+if __name__ == 'A__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(raw_input())
@@ -49,3 +52,6 @@ if __name__ == '__main__':
     fptr.write(str(res) + '\n')
 
     fptr.close()
+
+
+
