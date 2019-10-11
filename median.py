ea75504f295ff4725583d47a7211dfbedb1586cf
raghavan <raghavan@riptideio.com>
2019-10-11 06:57:09 +0530

    master: fixed issues

--- median.py
+++ median.py
@@ -1,19 +1,29 @@
 #!/bin/python
 
 import sys
-import statistics
+import bisect
 
-def median_of_list(lst):
-    return statistics.median(lst)
-    n = len(lst)
-    s = sorted(lst)
-    return (sum(s[n//2-1:n//2+1])/2.0, s[n//2])[n % 2] if n else None
+def median_of_list(s):
+    n = len(s)
+    #s = sorted(lst)
+    if n == 1:
+        return s[0]
+
+    if n == 2:
+        return float(s[0]+s[1])/2
+
+    if n%2 != 0:
+        return s[n/2]
+    else:
+        a = s[n/2]
+        b = s[(n/2)-1]
+        return float(a+b)/2
 
 def print_it(n):
-    if int(n) == n:
-        print(int(n))
-    else:
-        print('{0:g}'.format(n))
+    mm = '{0:f}'.format(n)
+    mm = mm.rstrip('0')
+    mm = mm.rstrip('.')
+    print(mm)
 
 
 def median(a,x):
@@ -21,20 +31,51 @@ def median(a,x):
     arr = zip(a,x)
     elem_list = []
     elem_dict = {}
-    for ta, tx in arr:
-        # print(ta,tx)
+    #for ta, tx in arr:
+    count  = 0
+    while count < len(a):
+        ta = a[count]
+        tx = x[count]
+        #print(ta,tx)
+        '''
+        if tx == 936866298:
+            print(tx)
+            print("tx in elem_dict{}".format(tx not in elem_dict))
+        '''
+
         if ta == 'r':
             if tx not in elem_dict:
                 print("Wrong!")
-                continue
             else:
-                del elem_dict[tx]
-                elem_list.remove(tx)
+                occ = elem_dict[tx]
+                occ -= 1
+                if occ == 0:
+                    del elem_dict[tx]
+                else:
+                    elem_dict[tx] = occ
+
+                ind = bisect.bisect_left(elem_list,tx)
+                del elem_list[ind]
+                if len(elem_list) == 0:
+                    print("Wrong!")
+                else:
+                    m = median_of_list(elem_list)
+                    print_it(m)
         else:
-            elem_dict[tx] = 999
-            elem_list.append(tx)
-        m = median_of_list(elem_list)
-        print_it(m)
+            if tx in elem_dict:
+                elem_dict[tx] += 1
+            else:
+                elem_dict[tx] = 1
+
+            bisect.insort(elem_list,tx)
+            #print(elem_list)
+            m = median_of_list(elem_list)
+            print_it(m)
+
+        count += 1
+        #print("------------")
+        #if count == 6:
+        #   break
         
 
 N = input()
