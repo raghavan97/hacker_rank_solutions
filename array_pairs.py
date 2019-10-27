7aae46c52bf8c7662e221aa1ce51165b00a48d3a
raghavan <raghavan@riptideio.com>
2019-10-27 15:51:34 +0530

    developing theme

--- array_pairs.py
+++ array_pairs.py
@@ -8,36 +8,47 @@ import sys
 
 # Complete the solve function below.
 def solve(arr):
-    #print(arr)
+    print(arr)
     count = 0
     match = 0
     n = len(arr)
+    print(n)
     N = n-1
-    total_combi = N*(N+1)/2
-
-    max_vals={}
-
-    while count < n:
+    count=N
+    no_of_tests=0
+    while count >= 0:
         i = count
         ai = arr[count]
-        j =  count + 1
-        max_ij = arr[i]
-        expected_min = max_ij/ai
-        while j < n:
+        j =  count - 1
+        if j >= 0:
+            max_val = arr[i]
+        while j >= 0:
+            if arr[j] > max_val:
+                max_val = arr[j]
+            min_num = max_val/arr[i]
+
+            # there should be atleast one number less than or equal to min_num
+            # on the LHS of j , if there is none , we can break
+            if j:
+                min_lhs = min(arr[0:j])
+            '''
+            if sarr[0] > min_num:
+                #print("{} < {} breaking out".format(min_num, sarr[0]))
+                break
+            '''
             # a[i] * a[j] <= max(a[i],a[i+1],.....a[j])
-            if arr[j] > max_ij:
-                max_ij = arr[j]
-                expected_min = max_ij/ai
-
-            if arr[j] <= expected_min:
+            no_of_tests += 1
+            mflag = False
+            if arr[j]*arr[i] <= max_val:
                 match += 1
-            #print("comparing arr[i]={} arr[j]={} <= expected_min={} match={}".format(ai, arr[j], expected_min,match))
+                mflag = True
+            print("trying {},{} max={} min={} min_lhs={} match={}".format(arr[j], ai, max_val, min_num, min_lhs, mflag))
+            j -= 1
+        count -= 1
 
-            j += 1
-        count += 1
 
-
-    #print(match)
+    print(no_of_tests)
+    print(match)
     return match
 
 
