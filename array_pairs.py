26b8219e6d3bb7e5f5117d72b3d2d1489b4d0f80
Raghavan Viswanathan <raghavan@Raghavans-MacBook-Air-2.local>
2019-10-28 13:16:04 +0530

    buckets

--- array_pairs.py
+++ array_pairs.py
@@ -6,42 +6,108 @@ import random
 import re
 import sys
 
+class Bucket(object):
+    def __init__(self, max_val, max_val_index):
+        self.max_val = max_val
+        self.max_val_index = max_val_index
+        self.values = []
+
+    def add_value(self, val):
+        self.values.append(val)
+
+    def __repr__(self):
+        bstr = "values={} max_val_index={} max_val={}".format(
+            self.values, self.max_val_index, self.max_val
+        )
+        return bstr
+
+
+
 class ArrayPairs(object):
     def __init__(self, arr):
         self.arr = arr
         self.arr_len = len(arr)
+        self.match = 0
 
     def populate_DS(self):
         print(self.arr)
         i = 0
+        buckets = []
         max_val = -1
-        max_val_arr = dict()
         while i < self.arr_len:
             if self.arr[i] > max_val:
                 max_val = self.arr[i]
-                max_val_arr[i] = {'max_val': max_val, 'values': []}
-                max_val_index = i
+                b = Bucket(max_val, i)
+                buckets.append(b)
 
-            max_val_arr[max_val_index]['values'].append(self.arr[i])
+            b.add_value(self.arr[i])
             i += 1
 
-
-        self.max_val_arr = max_val_arr
-        self.max_val_arr_keys = self.max_val_arr.keys()
-        self.max_val_arr_keys.sort()
-        for i in self.max_val_arr_keys:
-            self.max_val_arr[i]['values'].sort()
-            print(i,max_val_arr[i])
-
-    def solve2(self):
-        pass
+        for b in buckets:
+            print(b)
+        self.buckets = buckets
 
 
     def solve(self):
-        pass
+        self.calc_match(self.arr)
+
+    def calc_match_buckets(self, b1, b2):
+        match = 0
+        b1_values = sorted(b1.values)
+        for i in b1_values:
+            b2_values = sorted(b2.values)
+            for j in b2_values:
+                print("Trying {},{}".format(i,j))
+                if i*j <= b2.max_val:
+                    match += 1
+                else:
+                    break
+        return match
+
+
+
+    def solve2(self):
+        i = 0
+        n = len(self.buckets)
+        while i < n:
+            j = i + 1
+            bi = self.buckets[i]
+            self.calc_match(bi.values)
+            while j < n:
+                bj = self.buckets[j]
+                mat = self.calc_match_buckets(bi,bj)
+                self.match += mat
+                j += 1
+            i += 1
+
+
+
+
+    def calc_match(self, li_values):
+        i = 0
+        n = len(li_values)
+        a = li_values
+        while i < n:
+            ai = li_values[i]
+            j = i + 1
+            max_val = a[i]
+            while j < n:
+                if a[j] > max_val:
+                    max_val = a[j]
+                aj = a[j]
+                print("trying {},{} max_val={}".format(ai,aj,max_val))
+                if a[i]*a[j] < max_val:
+                    self.match += 1
+                j += 1
+            i += 1
+
+
 
     def run(self):
         self.populate_DS()
+        self.solve2()
+        print(self.match)
+        return self.match
 
 
 
@@ -51,6 +117,7 @@ def solve(arr):
     ap = ArrayPairs(arr)
     ap.run()
 
+#solve([1,3000, 28, 25, 4000, 15, 10, 6000, 24, 23, 8000, 25, 20])
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
