71ad7bdbda47e396ef55f971ee7ae4fcec588247
raghavan <raghavan@riptideio.com>
2019-11-03 17:01:29 +0530

    Works for all except 1

--- array_pairs.py
+++ array_pairs.py
@@ -29,14 +29,33 @@ class Bucket(object):
 
 class ArrayPairs(object):
     def __init__(self, arr):
-        self.arr = arr
-        self.arr_len = len(arr)
+        self.orig_arr = arr
+        self.orig_arr_len = len(arr)
         self.match = 0
-        self.cache_hits = 0
-        self.cache = {}
+        self.arr = []
 
 
     def populate_DS(self):
+
+        i=0
+        n = len(self.orig_arr)
+        no_of_ones = 0
+        while i < n:
+            if self.orig_arr[i] == 1:
+                no_of_ones += 1 
+            else:
+                self.arr.append(self.orig_arr[i])
+            i += 1
+
+        no_of_terms = no_of_ones
+        highest = len(self.orig_arr) - 1
+        least = highest - no_of_terms + 1
+        self.match += ((no_of_terms)*(highest+least))/2
+        #print(self.match)
+
+        #self.arr = self.orig_arr
+        self.arr_len = len(self.arr)
+
         #print(self.arr)
         i = 0
         buckets = []
@@ -54,56 +73,12 @@ class ArrayPairs(object):
             b.store_sorted_values()
         self.buckets = buckets
 
-    def get_cache(self, b1, b2):
-        b1_len = len(b1.sorted_values)
-        b2_len = len(b2.sorted_values)
-        if b1_len < b2_len:
-            key_list = [(b1.sorted_values,b2.sorted_values)]
-        elif b1_len > b2_len:
-            key_list = [(b2.sorted_values,b1.sorted_values)]
-        else:
-            key_list = [
-                (b1.sorted_values,b2.sorted_values),
-                (b2.sorted_values,b1.sorted_values)
-            ]
-        for k in key_list:
-            k0 = tuple(k[0])
-            k1 = tuple(k[1])
-            key = (k0,k1)
-            if self.cache.get(key):
-                return self.cache[key]
-        return None
-
-    def set_cache(self, b1, b2, matches):
-        b1_len = len(b1.sorted_values)
-        b2_len = len(b2.sorted_values)
-        if b1_len < b2_len:
-            key_list = [(b1.sorted_values,b2.sorted_values)]
-        elif b1_len > b2_len:
-            key_list = [(b2.sorted_values,b1.sorted_values)]
-        else:
-            key_list = [
-                (b1.sorted_values,b2.sorted_values),
-                (b2.sorted_values,b1.sorted_values)
-            ]
-        for k in key_list:
-            k0 = tuple(k[0])
-            k1 = tuple(k[1])
-            key = (k0,k1)
-            self.cache[key] = matches
-
 
     def calc_match_buckets(self, b1, b2):
         match = 0
-        ret = self.get_cache(b1,b2)
-        if ret is not None:
-            self.cache_hits += 1
-            return ret
-        print(self.cache)
         for i in b1.sorted_values:
             cnt = bisect.bisect_right(b2.sorted_values, b2.max_val/i)
             match += cnt
-        ret = self.set_cache(b1,b2,match)
         return match
 
     def solve2(self):
@@ -125,6 +100,9 @@ class ArrayPairs(object):
         n = len(li_values)
         a = li_values
         match = 0
+
+        if sorted_values[0] == sorted_values[len(sorted_values)-1]:
+            return 0
 
         if n > 50:
             num = bisect.bisect_right(sorted_values,1)
@@ -152,7 +130,6 @@ class ArrayPairs(object):
     def run(self):
         self.populate_DS()
         self.solve2()
-        print("Cache hits={}".format(self.cache_hits))
         return self.match
 
 # Complete the solve function below.
@@ -171,10 +148,10 @@ def debug_main(fname):
     fptr.write(str(result) + '\n')
     fptr.close()
 
-debug_main(sys.argv[1])
+#debug_main(sys.argv[1])
 
 #solve([1,3000, 28, 25, 4000, 15, 10, 6000, 24, 23, 8000, 25, 20])
-if __name__ == 'b__main__':
+if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     arr_count = int(raw_input())
